3bc8c9cb5b0c463068c600d7dd8749c309994675
Xeyn19 <edgarrodilorosa@gmail.com>
2026-03-05 14:44:34 +0800

    added useEffect polling in fetching data.

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,8 @@
 export const recipes = [
     {id:1, name: "Iced Coffee"},
-    {id:2, name: "Iced Coffee Mocha"}
+    {id:2, name: "Iced Coffee Mocha"},
+    {id:3, name: "Iced Vanilla"},
+    {id:4, name: "Iced Matcha"},
+
+  
 ]
--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -3,19 +3,27 @@ import React, { useEffect, useState } from 'react'
 const Recipes = () => {
     const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        const fetchdata = async () => {
-            try{
-                const response = await fetch('http://localhost:8000/api/recipes')
-                const data = await response.json();
-                setRecipes(data);
-            }catch(error){
-                console.error('Error Fetching Recipes Data', error)
-            }
+   const fetchdata = async () => {
+        try{
+            const response = await fetch('http://localhost:8000/api/recipes');
+            const data = await response.json();
+            setRecipes(data);
+        }catch(error){
+            console.error('Error Fetching Recipes Data', error)
         }
+    }
 
+    useEffect(() => {
         fetchdata();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchdata();
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
   return (
     <div className='flex justify-center bg-slate-200 flex-col min-h-screen items-center'>
         <h1 className='my-5'>Recipes</h1>
